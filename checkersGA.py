80b727089ee1707c3f01b1fbda3022baa3baf92a
hezii <genesis.alvarez@uamaker.nyc>
2019-03-15 22:12:13 -0400

    row of squares
    using a function, a row of different colored squares is made (8 of them).

--- checkersGA.py
+++ checkersGA.py
@@ -1,19 +1,26 @@
 from graphics import * #imports graphics module
 
-sqX = 0 #inti variable
-sqY = 0 #inti variable
-
 print("""Hello! This is a program that will create a checkerboard.
 Please type in the size you want the board to be.""") #introduces user to program and asks user size of board
 print(" ") #empty print line for readabillity
 sqSz = int(input("Size >>> ")) #waiting for user input of square size (sqSize)
+print("Now I will need the color that you would like the board to be.") #asks user color of board (not black tiles)
+color = str(input("Color >>> ")) #waiting for user input of color of square (color)
+
+def sq_draw (sqX, sqY, color, size, win): #defining the fuction to draw a square (using x,y coords; color; square size; window)
+    square = Rectangle(Point(sqX, sqY), Point(sqX + size, sqY + size)) #points to draw square
+    square.setFill(color) #color of the square
+    square.draw(chkWin) #draws the actual square
 
 chkWin = GraphWin("Checkers", sqSz * 10, sqSz * 10) #creating the checkers window
 chkWin.setCoords(0,0, sqSz * 10, sqSz * 10) #setting the coords of the window
 
-square = Rectangle(Point(sqX, sqY), Point(sqX + sqSz, sqY + sqSz)) #points to draw square
-square.setFill("black") #color of the square
-square.draw(chkWin) #draws the actual square
+for i in range(8): 
+    if i % 2 == 0: 
+        sqC = color #set the square color to user input
+    else: #if remainder is not 0 then
+        sqC = "black" #set the square color to black
+    sq_draw(sqSz * (i + 1), sqSz, sqC, sqSz, chkWin) #draws square with user color and size
 
 chkWin.getMouse()
 chkWin.close()
